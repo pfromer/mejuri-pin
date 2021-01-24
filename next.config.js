77022162bd4bcd10abb7bcaeee1697fc2857565f
Pablo Fromer <pablo.fromer@trocafone.com>
2021-01-24 12:58:53 -0300

    redirect on 404

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
     images: {
         domains: ['dto508s2j2p46.cloudfront.net']
     },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/category/shop_all',
+                permanent: true,
+            },
+        ]
+    },
+
 }
