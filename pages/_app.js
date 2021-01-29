d4a375a334989aa45636532ece1f260d9305ae25
Pablo Fromer <pablo.fromer@trocafone.com>
2021-01-29 12:05:23 -0300

    renombre index.css porque no me lo toma vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
-import '../public/index.css';
+import '../public/_index.css';
 import Theme from '../utilities/theme';
 import { PersistGate } from 'redux-persist/integration/react';
 import { wrapper } from '../redux/store';
@@ -13,16 +13,16 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-    const store = useStore((state) => state);
-    return (
-        <Provider store={store}>
-            <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-                <Theme>
-                    <Component {...pageProps} />
-                </Theme>
-            </PersistGate>
-        </Provider>
-    );
+  const store = useStore((state) => state);
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+        <Theme>
+          <Component {...pageProps} />
+        </Theme>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
